f1d6031974c48505c4d6b4cd844afd9b6ab56a12
Frzkyy <fairuzzacky@gmail.com>
2026-04-29 22:34:59 +0700

    test

--- rust/testing/src/main.rs
+++ rust/testing/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let x: i32 = 200;
+    let y: i32 = 32;
+    println!("x * y = {}",x*y);
+    let z = ("Rotte", 200, 300, 400);
+    println!("{:?} Ini Hasilnya ya", z);
 }
